c1830054256b13b101340e1b8b62ac603f68d5cb
Romenick Garcia <85664536+keanfinity@users.noreply.github.com>
2022-05-06 22:41:58 +0800

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -18,23 +18,23 @@ const Main=()=>{
     },[url_set])
 
     const getData=(movieType)=>{
-        if(movieType=="Featured")
+        if(movieType==="Featured")
         {
             url=base_url+"/discover/movie?sort_by=popularity.desc"+API_key;
         }
-        if(movieType=="Theatre")
+        if(movieType==="Theatre")
         {
             url=base_url+"/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22"+API_key;
         }
-        if(movieType=="Kids")
+        if(movieType==="Kids")
         {
             url=base_url+"/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc"+API_key;
         }
-        if(movieType=="Drama")
+        if(movieType==="Drama")
         {
             url=base_url+"/discover/movie?with_genres=18&primary_release_year=2014"+API_key;
         }
-        if(movieType=="Comedy")
+        if(movieType==="Comedy")
         {
             url=base_url+"/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc"+API_key;
         }
@@ -42,7 +42,7 @@ const Main=()=>{
 
     }
     const searchMovie=(evt)=>{
-        if(evt.key=="Enter")
+        if(evt.key==="Enter")
         {
             url=base_url+"/search/movie?api_key=db95773a7fb212ba790d71f6adac0e7e&query="+search;
             setUrl(url);
