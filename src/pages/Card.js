7ee4394d280bca024175b1a717c09ec55a44b573
Romenick Garcia <85664536+keanfinity@users.noreply.github.com>
2022-05-06 23:06:00 +0800

    Update Card.js

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -6,7 +6,7 @@ const Card=(movie)=>{
     return(
         <>
             <div className="movie">
-                <img src={img_path+movie.info.poster_path} className="poster"></img>
+                <img alt="" src={img_path+movie.info.poster_path} className="poster"></img>
                 <div className="movie-details">
                     <div className="box">
                         <h4 className="title">{movie.info.title}</h4>
